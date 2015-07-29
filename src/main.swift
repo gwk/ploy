13421cf9d821044ed61a0f8a4447b05e2ef500cc
George King <george.w.king@gmail.com>
2015-07-29 18:04:43 -0400

    basic io.

--- src/main.swift
+++ src/main.swift
@@ -1,2 +1,9 @@
 // Copyright © 2015 gwk. Permission to use this file is granted in ploy/license.txt.
 
+
+for (i, arg) in Process.arguments.enumerate() {
+  if i > 0 {
+    let src = InFile(path: arg).read()
+    errL(src)
+  }
+}
