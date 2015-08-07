e90f2d9943b477f3c0cba7b76d142b8d95d473a4
George King <george.w.king@gmail.com>
2015-08-07 11:57:43 -0400

    qk submodule.

--- src/check.swift
+++
@@ -1,8 +0,0 @@
-// Copyright © 2015 gwk. Permission to use this file is granted in ploy/license.txt.
-
-
-func check(condition: Bool, @autoclosure _ message: () -> String, file: StaticString = __FILE__, line: UWord = __LINE__) {
-  if !condition {
-    fatalError(message, file: file, line: line)
-  }
-}
