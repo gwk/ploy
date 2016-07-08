9676eda36dc8426a0d4762c55320f66779c2346d
George King <george.w.king@gmail.com>
2016-07-08 16:45:24 -0400

    Place.

--- src/Bind.swift
+++ src/Bind.swift
@@ -3,53 +3,39 @@
 
 class Bind: Form { // value binding: `name=expr`.
 
-  enum Name {
-    case sym(Sym)
-    case ann(Ann)
-
-    var sym: Sym {
-      switch self {
-        case sym(let sym): return sym
-        case ann(let ann):
-          guard case .sym(let sym) = ann.expr else { fatalError() }
-          return sym
-      }
-    }
-  }
-
-  let name: Name
+  let place: Place
   let val: Expr
   
-  init(_ syn: Syn, name: Name, val: Expr) {
-    self.name = name
+  init(_ syn: Syn, place: Place, val: Expr) {
+    self.place = place
     self.val = val
     super.init(syn)
   }
   
   static func mk(l: Form, _ r: Form) -> Form {
-    let name: Name
+    let place: Place
     if let sym = l as? Sym {
-      name = .sym(sym)
+      place = .sym(sym)
     }
     else if let ann = l as? Ann {
       guard case .sym = ann.expr else {
         ann.expr.form.failSyntax("annotated binding name expects name symbol but received \(ann.expr.form.syntaxName).")
       }
-      name = .ann(ann)
+      place = .ann(ann)
     } else {
       l.failSyntax("binding name expects name symbol or annotated name symbol but received \(l.syntaxName).")
     }
     return Bind(Syn(l.syn, r.syn),
-      name: name,
+      place: place,
       val: Expr(form: r, subj: "binding", exp: "value expression"))
   }
   
   override func write<Stream : OutputStream>(to stream: inout Stream, _ depth: Int) {
     writeHead(to: &stream, depth, "\n")
-    sym.write(to: &stream, depth + 1)
+    place.write(to: &stream, depth + 1)
     val.write(to: &stream, depth + 1)
   }
 
-  var sym: Sym { return name.sym }
+  var sym: Sym { return place.sym }
 }
 
---
+++ src/Place.swift
@@ -0,0 +1,35 @@
+// Copyright © 2016 George King. Permission to use this file is granted in ploy/license.txt.
+
+
+enum Place: SubForm { // left side of a binding.
+
+  case sym(Sym)
+  case ann(Ann)
+
+
+  init(form: Form, subj: String) {
+    if let form = form as? Sym {
+      self = .sym(form)
+    } else if let form = form as? Ann {
+      self = .ann(form)
+    } else {
+      form.failSyntax("\(subj) expects accessor symbol or number literal but received \(form.syntaxName).")
+    }
+  }
+
+  var form: Form {
+    switch self {
+      case .sym(let sym): return sym
+      case .ann(let ann): return ann
+    }
+  }
+
+    var sym: Sym {
+      switch self {
+        case sym(let sym): return sym
+        case ann(let ann):
+          guard case .sym(let sym) = ann.expr else { fatalError() }
+          return sym
+      }
+    }
+  }
