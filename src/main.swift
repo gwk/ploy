61e99cfc2e4e3e93c82f3f40860f6e2f85451fb2
George King <george.w.king@gmail.com>
2016-11-13 13:35:02 -0500

    wrapper main() func to reduce spurious swift errors.

--- src/main.swift
+++ src/main.swift
@@ -10,48 +10,54 @@ let validOpts = Set([
   "-o",
 ])
 
-var ployPath: String! = nil
-var includePaths: [String] = []
-var libPaths: [String] = []
-var opts: [String: String] = [:]
 
-var opt: String? = nil
-for (i, arg) in processArguments.enumerated() {
-  if i == 0 {
-    ployPath = arg
-  } else if let o = opt {
-    opts[o] = arg
-    opt = nil
-  } else if validOpts.contains(arg) {
-    opt = arg
-  } else if arg.hasPrefix("-") {
-    fail("unrecognized option: '\(arg)'")
-  } else if arg.pathExt == ".js" {
-    includePaths.append(arg)
-  } else if arg.pathExt == ".ploy" {
-    libPaths.append(arg)
-  } else {
-    fail("invalid path extension: '\(arg)'")
+func main() {
+  var ployPath: String! = nil
+  var includePaths: [String] = []
+  var libPaths: [String] = []
+  var opts: [String: String] = [:]
+
+  var opt: String? = nil
+  for (i, arg) in processArguments.enumerated() {
+    if i == 0 {
+      ployPath = arg
+    } else if let o = opt {
+      opts[o] = arg
+      opt = nil
+    } else if validOpts.contains(arg) {
+      opt = arg
+    } else if arg.hasPrefix("-") {
+      fail("unrecognized option: '\(arg)'")
+    } else if arg.pathExt == ".js" {
+      includePaths.append(arg)
+    } else if arg.pathExt == ".ploy" {
+      libPaths.append(arg)
+    } else {
+      fail("invalid path extension: '\(arg)'")
+    }
+  }
+  let _ = ployPath
+
+  check(opt == nil, "dangling option flag: '\(opt)'")
+
+  guard let mainPath = opts["-main"] else { fail("`-main main-src-path` argument is required.") }
+  guard let outPath = opts["-o"] else { fail("`-o out-path` argument is required.") }
+
+  let tmpPath = outPath + ".tmp"
+  let tmpFile = guarded { try OutFile(path: tmpPath, create: 0o644) }
+
+  let (mainIns, mainIn) = Src(path: mainPath).parseMain(verbose: false)
+
+  let ins = mainIns + libPaths.flatMap { Src(path: $0).parseLib(verbose: false) }
+
+  compileProgram(file: tmpFile, includePaths: includePaths, ins: ins, mainIn: mainIn)
+
+  renameFileAtPath(tmpPath, toPath: outPath)
+  do {
+    try File.changePerms(outPath, 0o755)
+  } catch let e {
+    fail("error: could not set compiled output file to executable: \(outPath)\n  \(e)")
   }
 }
 
-check(opt == nil, "dangling option flag: '\(opt)'")
-
-guard let mainPath = opts["-main"] else { fail("`-main main-src-path` argument is required.") }
-guard let outPath = opts["-o"] else { fail("`-o out-path` argument is required.") }
-
-let tmpPath = outPath + ".tmp"
-let tmpFile = guarded { try OutFile(path: tmpPath, create: 0o644) }
-
-let (mainIns, mainIn) = Src(path: mainPath).parseMain(verbose: false)
-
-let ins = mainIns + libPaths.flatMap { Src(path: $0).parseLib(verbose: false) }
-
-compileProgram(file: tmpFile, includePaths: includePaths, ins: ins, mainIn: mainIn)
-
-renameFileAtPath(tmpPath, toPath: outPath)
-do {
-  try File.changePerms(outPath, 0o755)
-} catch let e {
-  fail("error: could not set compiled output file to executable: \(outPath)\n  \(e)")
-}
+main()
