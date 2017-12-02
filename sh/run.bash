af0f93ffa172513ee118b81a25e89d085462648a
George King <george.w.king@gmail.com>
2017-12-02 13:12:07 -0500

    run.sh automatically tries to add .ploy extension.

--- sh/run.bash
+++ sh/run.bash
@@ -1,17 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 set -e
 cd $(dirname $0)/..
 
-make build
+error() { echo $@ 1>&2; exit 1; }
 
 mainPath="$1"; shift || mainPath="test/_repro.ploy"
+
+if [[ ! -f "$mainPath" ]]; then
+  if [[ -f "$mainPath"ploy ]]; then # tab completion often stops at the dot because of multiple ploy, err, iot extensions.
+    mainPath="$mainPath"ploy
+  else
+    error "source file does not exist: $mainPath"
+  fi
+fi
+
 stem=${mainPath%.ploy}
 outPath="_build/$stem.js"
 outDir=$(dirname "$outPath")
 
 mkdir -p "$outDir"
+make build
 set -x
 _build/debug/ploy lib -mapper ./gen-source-map -main "$mainPath" -o "$outPath"
 "$outPath" "$@"
